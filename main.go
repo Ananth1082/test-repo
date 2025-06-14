c10db4a2ef70558d20135e743d0bf5a2ca723eca
Ananth1082 <ananthshetty203@gmail.com>
2025-06-14 12:12:30 +0530

    [featfeat] added message route

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 //go:embed index.html
@@ -13,15 +17,53 @@ func main() {
 	fmt.Println("Serving HTML content: ", html)
 
 	router := http.NewServeMux()
+
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Recieved request from client")
 		w.WriteHeader(http.StatusOK)
-		if _, err := fmt.Fprintf(w, html); err != nil {
-			fmt.Println("Error writing response: ", err)
+		if _, err := fmt.Fprint(w, html); err != nil {
+			log.Println("Error writing response: ", err)
 		}
 	})
+
+	router.HandleFunc("GET /message", func(w http.ResponseWriter, r *http.Request) {
+		if fr, err := os.ReadFile("messages.log"); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "<p>No messages!</p>")
+		} else {
+			messsages := string(fr)
+			str := `<table border="1">`
+			for i, msg := range strings.Split(messsages, "\n") {
+				if msg == "" {
+					continue
+				}
+				str += fmt.Sprintf("<tr><td>%d</td><td>%s</td></tr>", i+1, msg)
+			}
+			str += "</table>"
+			fmt.Fprintln(w, str)
+		}
+	})
+
+	router.HandleFunc("POST /message", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Error reading request body: ", err)
+			return
+		}
+
+		fileWriter, err := os.OpenFile("messages.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println("Error opening log file: ", err)
+			return
+		}
+		if _, err := fmt.Fprintln(fileWriter, string(data)); err != nil {
+			log.Println("Error writing logs: ", err)
+		}
+	})
+
 	fmt.Println("Starting server...")
-	if err := http.ListenAndServe(":8800", router); err != nil {
+	if err := http.ListenAndServe(":8134", router); err != nil {
 		panic("Server couldnt start")
 	}
 }
